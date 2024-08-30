b03ebef85a3b458eb22de249b020033c35ec7744
Mpt107 <mpereztureo@gmail.com>
2024-08-29 20:37:38 -0400

    modificacion del dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Muchas gracias por tu mensaje, respondere a la brevedad!');
-  document.getElementById('contactForm').reset();
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('header');
+  const contactButton = document.getElementById('contactButton');
+
+  header.addEventListener('mouseover', function() {
+      contactButton.style.display = 'block';
+  });
+
+  header.addEventListener('mouseout', function() {
+      contactButton.style.display = 'none';
+  });
+
+  contactButton.addEventListener('click', function() {
+      const contactTab = document.querySelector('a[href="#contact"]');
+      if (contactTab) {
+          contactTab.click();
+      }
+  });
 });
+
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Muchas gracias por tu mensaje, responderé a la brevedad!');
+    document.getElementById('contactForm').reset();
+  });
+
